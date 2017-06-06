ec91688455c5042c4d97ae1c559e56ff70f76835
yk.izk <yk.izk.private@gmail.com>
2017-06-06 23:42:35 +0900

    add make_deck method to dealer class

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -5,22 +5,25 @@ CARD_INFO = {
 
 class Dealer
   def initialize
-    @deck = []
-
-    # make a deck
-    CARD_INFO[:suits].each do |suit|
-      CARD_INFO[:numbers].each do |numbers|
-        @deck << Card.new(suit, numbers)
-      end
-    end
-
-    # shuffle the deck
-    @deck.shuffle!
+    @deck = make_deck
   end
 
   # returns a card on the top of the deck and delete it from the deck
   def deal
     @deck.shift
+  end
+
+  def make_deck
+    deck = []
+
+    # make a deck
+    CARD_INFO[:suits].each do |suit|
+      CARD_INFO[:numbers].each do |numbers|
+        deck << Card.new(suit, numbers)
+      end
+    end
+
+    deck.shuffle!
   end
 
   attr_reader :deck
